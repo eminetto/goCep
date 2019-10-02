1fe513d6a094e0fc57b6ac8902c52c5406f5ae96
Elton Minetto <eminetto@gmail.com>
2019-10-01 22:02:00 -0300

    feat: update to Go 1.13 (#13)
    * feat: update to Go 1.13
    
    * feat: add travis

--- server.go
+++ server.go
@@ -1,62 +1,97 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	curl "github.com/andelf/go-curl"
+	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/ryanuber/go-filecache"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"time"
-	"errors"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const serverPort = "3000";
 const cacheTime = 500;
 
-func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello world!"))
-	})
-	router.HandleFunc("/cep/{id}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-		writer.Header().Set("Content-Type", "application/json")
-		writer.Write([]byte(getCep(vars["id"])))
-	})
-
-	http.ListenAndServe(":"+serverPort, router)
+type Cep struct {
+	Cep string `json:"cep"`
+	Logradouro string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro string `json:"bairro"`
+	Localidade string `json:"localidade"`
+	Uf string `json:"uf"`
+	Unidade string `json:"unidade"`
+	Ibge string `json:"ibge"`
+	Gia string `json:"gia"`
 }
 
-func getCep(id string) string {
+func main() {
+	errorMessage := "Erro lendo CEP"
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		_, err := rw.Write([]byte([]byte("ping")))
+		if err != nil {
+			respondWithError(rw, http.StatusUnauthorized, err.Error(), errorMessage)
+			return
+		}
+	})
+	router.HandleFunc("/cep/{id}", func(rw http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		rw.Header().Set("Content-Type", "application/json")
+		cep, err := getCep(vars["id"])
+		if err != nil {
+			respondWithError(rw, http.StatusUnauthorized, err.Error(), errorMessage)
+			return
+		}
+		_, err = rw.Write([]byte(cep))
+		if err != nil {
+			respondWithError(rw, http.StatusUnauthorized, err.Error(), errorMessage)
+			return
+		}
+	})
+	http.Handle("/", router)
+	logger := log.New(os.Stderr, "logger: ", log.Lshortfile)
+	srv := &http.Server{
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Addr:         ":"+serverPort,
+		Handler:      context.ClearHandler(http.DefaultServeMux),
+		ErrorLog:     logger,
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getCep(id string) (string, error) {
 	cached := getFromCache(id)
 	if cached != "" {
-		return cached
+		return cached, nil
 	}
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-
-	easy.Setopt(curl.OPT_URL, "http://viacep.com.br/ws/"+id+"/json/")
-
-	result := " "
-
-	// make a callback function
-	fooTest := func(buf []byte, userdata interface{}) bool {
-		result = string(buf)
-
-		return true
+	req, err := http.Get(fmt.Sprintf("http://viacep.com.br/ws/%s/json/", id))
+	if err != nil {
+		return "", err
 	}
 
-	easy.Setopt(curl.OPT_WRITEFUNCTION, fooTest)
-
-	if err := easy.Perform(); err != nil {
-		fmt.Printf("ERROR: %v\n", err)
+	var c Cep
+	err = json.NewDecoder(req.Body).Decode(&c)
+	if err != nil {
+		return "", err
+	}
+	res, err := json.Marshal(c)
+	if err != nil {
+		return "", err
 	}
 
-	return saveOnCache(id, result)
+	return saveOnCache(id, string(res)), nil
 }
 
 func getFromCache(id string) string {
@@ -81,7 +116,6 @@ func getFromCache(id string) string {
 
 func saveOnCache(id string, content string) string {
 	updater := func(path string) error {
-		println("updater")
 		f, err := os.Create(path)
 		if err != nil {
 			return err
@@ -102,5 +136,18 @@ func saveOnCache(id string, content string) string {
 }
 
 func getCacheFilename(id string) string {
-  return os.TempDir()+"/cep"+strings.Replace(id, "-", "", -1);
+  return os.TempDir()+"/cep"+strings.Replace(id, "-", "", -1)
+}
+
+//RespondWithError return a http error
+func respondWithError(w http.ResponseWriter, code int, e string, message string) {
+	respondWithJSON(w, code, map[string]string{"code": strconv.Itoa(code), "error": e, "message": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
